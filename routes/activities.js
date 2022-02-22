5f36c7e1451c30b6d233601b6a451825ee2a74be
Emanuel Arroyo <emanuelarroyodev@gmail.com>
2022-02-22 20:39:43 -0300

    CONTEMPLATED FEEDBACK

--- routes/activities.js
+++ routes/activities.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 
 // Middleware validations required
-const activityValidation = require("../validations/activity.validations");
+const activityValidation = require("../middlewares/activity.validations");
 
 // Controller Api required
 const { store } = require("../controllers/activity.controller");
 
 // Routes
-router.post("/add", activityValidation, store);
+router.post("/", activityValidation, store);
 
 module.exports = router;
