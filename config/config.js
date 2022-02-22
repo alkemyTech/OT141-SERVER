fc947e4b2d1b5ca375c8e97c09e10e3c494ccec6
Nathan Delgado <nathandelgado.dv@gmail.com>
2022-02-22 15:32:31 -0500

    Create env of DB of tests

--- config/config.js
+++ config/config.js
@@ -10,10 +10,10 @@ module.exports = {
         "dialect": "mysql"
     },
     "test": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
+        "username": process.env.DB_USER_TEST,
+        "password": process.env.DB_PASSWORD_TEST,
+        "database": process.env.DB_NAME_TEST,
+        "host": process.env.DB_HOST_TEST,
         "dialect": "mysql"
     },
     "production": {
