eb658b075f2ddbc1c3284be9f57a4b4d0f4c0849
Nathan Delgado <nathandelgado.dv@gmail.com>
2022-02-22 23:06:57 -0500

    Clean route auth/login of validations

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,16 @@
 var express = require('express');
-const { check } = require('express-validator');
 const { userLogin } = require('../controllers');
-const { 
-    validateErrors,
-    isExistEmail
+const {
+    validationLogin,
+    validateErrors
 } = require('../middlewares');
-
 var router = express.Router();
 
 // Login of user auth/login
-router.post('/login', [
-    check('email', 'The value is required').not().isEmpty(),
-    check('password', 'The value is required').not().isEmpty(),
-    check('email', 'The value is invalid').isEmail(),
-    check('password', 'Requiered 8 min characters').isLength({ min: 8 }),
-    isExistEmail,
-    validateErrors
-], userLogin
+router.post('/login', 
+    validationLogin,
+    validateErrors,
+    userLogin
 );
 
 module.exports = router;
