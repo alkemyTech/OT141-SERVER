375f379cf537599dedaeca87b0607d97a4d5a01d
Nathan Delgado <nathandelgado.dv@gmail.com>
2022-02-22 23:03:31 -0500

    Add exist email to userLogin function

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,29 +8,36 @@ const db = require('../models');
 
 const userLogin = async(req, res) => {
     try {
-    const {email, password} = req.body;
+        const {email, password} = req.body;
     
-    const user = await db.User.findOne({
+        const user = await db.User.findOne({
         where: {email}
-    });
-
-    // Verify Password
-    const validPass = bcryptjs.compareSync(password, user.password);
-    if (!validPass) {
-        return res.status(400).json({
-            ok: false,
-            msg: "The passsword is wrong"
         });
-    }
+    
+        if (!user) {
+            return res.status(404).json({
+                ok: false,
+                msg: `The ${email} not exist`
+            });
+        }
 
-    //Create JWT
-    const token = await createJWT(user.email);
-
-    res.status(200).json({
-        ok: true,
-        msg: 'User logged in',
-        token
-    });
+        // Verify Password
+        const validPass = bcryptjs.compareSync(password, user.password);
+        if (!validPass) {
+            return res.status(400).json({
+                ok: false,
+                msg: "The passsword is wrong"
+            });
+        }
+    
+        //Create JWT
+        const token = await createJWT(user.email);
+    
+        res.status(200).json({
+            ok: true,
+            msg: 'User logged in',
+            token
+        });
     } catch (err) {
         resError(err, res);
     }
