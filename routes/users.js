7f5bf3542118383936706c452d46528a7a7679eb
fedapon <fedapon@gmail.com>
2022-02-24 23:37:43 -0300

    users patch endpoint created

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { update } = require("../controllers/user.controller"); 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* PATCH user */
+router.patch('/:id', update)
 
 module.exports = router;
