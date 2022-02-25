7b35af11affe0b3ad5e8826189ad7569ec368c83
fedapon <fedapon@gmail.com>
2022-02-24 22:07:47 -0300

    deleted console.log

--- helpers/verifyJWT.js
+++ helpers/verifyJWT.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 const verifyJWT = (token) => {
-    console.log('verify')
     return new Promise((resolve, reject) => {
         jwt.verify(token, process.env.SECRETORPRIVATEKEY, (err, authData) => {
             if (err) {
