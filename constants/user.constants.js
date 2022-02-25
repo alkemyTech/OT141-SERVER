d81c435117417bb37b932d3d2322800c841c52a4
fedapon <fedapon@gmail.com>
2022-02-25 20:51:27 -0300

    added ROLE_UNDEFINED

--- constants/user.constants.js
+++ constants/user.constants.js
@@ -1,7 +1,9 @@
-const ROLE_ADMIN = "1"
-const ROLE_USER = "2"
+const ROLE_UNDEFINED = 0
+const ROLE_ADMIN = 1
+const ROLE_USER = 2
 
 module.exports = {
+    ROLE_UNDEFINED,
     ROLE_ADMIN,
     ROLE_USER
 }
