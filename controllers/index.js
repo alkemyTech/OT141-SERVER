7078bdb0a94bfd301cd287a0c12af1fb682d56ad
Nathan Delgado <nathandelgado.dv@gmail.com>
2022-02-25 19:41:10 -0500

    Add functions to index

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
 const authControllers = require('./auth');
+const categoriesControllers = require('./categories');
 
 module.exports = {
-    ...authControllers,
-}
+  ...authControllers,
+  ...categoriesControllers,
+};
