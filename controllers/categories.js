20b1ed5c0df55e3d9a3cb86c63734148aef2cdf4
Nathan Delgado <nathandelgado.dv@gmail.com>
2022-03-01 11:41:47 -0500

    Delete check role of getAllCategories, send it to middleware

--- controllers/categories.js
+++ controllers/categories.js
@@ -3,57 +3,14 @@ const db = require('../models');
 const createCategory = async (req, res) => {
   try {
     const { name, description, image } = req.body;
-    const { roleId } = req.user;
     const nameUpperCase = name.toUpperCase();
-
-    // validate if user is admin
-    if (roleId !== 1) {
-      res.status(401).json({
-        ok: false,
-        msg: 'You do not have privileges for this',
-      });
-    }
-
-    // validate if exist description and img
-    if (description && image) {
-      const newCategory = await db.category.create({
-        name: nameUpperCase,
-        description,
-        image,
-      });
-
-      return res.status(201).json({
-        ok: true,
-        msg: `Created ${newCategory.name} category`,
-      });
-    }
-
-    if (description) {
-      const newCategory = await db.category.create({
-        name: nameUpperCase,
-        description,
-      });
-
-      return res.status(201).json({
-        ok: true,
-        msg: `Created ${newCategory.name} category`,
-      });
-    }
-    if (image) {
-      const newCategory = await db.category.create({
-        name: nameUpperCase,
-        image,
-      });
-
-      return res.status(201).json({
-        ok: true,
-        msg: `Created ${newCategory.name} category`,
-      });
-    }
 
     const newCategory = await db.category.create({
       name: nameUpperCase,
+      description: description || null,
+      image: image || null,
     });
+
     res.status(201).json({
       ok: true,
       msg: `Created ${newCategory.name} category`,
@@ -69,16 +26,6 @@ const createCategory = async (req, res) => {
 
 const getAllCategories = async (req, res) => {
   try {
-    const { roleId } = req.user;
-
-    // validate if user is admin
-    if (roleId !== 1) {
-      res.status(401).json({
-        ok: false,
-        msg: 'You do not have privileges for this',
-      });
-    }
-
     const categories = await db.category.findAll({
       attributes: ['name'],
     });
