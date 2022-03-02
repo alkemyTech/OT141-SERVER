b85a5a0784e153faba3e3c92182ac0000fd175c0
galantejulian <galante.julian@gmail.com>
2022-03-02 19:15:52 -0300

    added comments and small changes at users controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,20 @@
 const db = require("../models");
 
 module.exports = {
-    // DELETE USER = DELETE
+    // DELETE USER = SOFT DELETE
     deleteUser: async (req, res) => {
         try {
             const { id } = req.params;
-            const userToDelete = await db.User.findByPk(id);
-            if (!userToDelete) {
-                res.status(404).json({
-                    del: false,
-                    message: "it is not possible to delete that user since that id is not found in our database",
-                })
-            } else {
-                await db.User.destroy({
-                    where: {
-                        id
-                    }
-                })
-                res.status(200).json({
-                    del: true,
-                    message: `user with id ${id} was deleted successfully`,
-                })
-            }
+            // As paranoid at model is setted to true, this will make a soft delete;
+            await db.User.destroy({
+                where: {
+                    id
+                }
+            })
+            res.status(200).json({
+                del: true,
+                message: `user with id ${id} was deleted successfully`,
+            })
         } catch (error) {
             res.status(500).json({
                 del: false,
@@ -30,4 +23,3 @@ module.exports = {
         }
     }
 }
-
