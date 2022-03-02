464c2536c6280e9f5b90a5a247155d125088aced
galantejulian <galante.julian@gmail.com>
2022-03-02 20:36:18 -0300

    delete funcionando sin errores

--- controllers/users.js
+++ controllers/users.js
@@ -5,16 +5,23 @@ module.exports = {
     deleteUser: async (req, res) => {
         try {
             const { id } = req.params;
-            // As paranoid at model is setted to true, this will make a soft delete;
-            await db.User.destroy({
+            // As paranoid at model is setted to true, destroy method will make a soft delete;
+            let userDeleted = await db.User.destroy({
                 where: {
                     id
                 }
             })
-            res.status(200).json({
-                del: true,
-                message: `user with id ${id} was deleted successfully`,
-            })
+            if (userDeleted == 1) {
+                res.status(200).json({
+                    del: true,
+                    message: `user with id ${id} was deleted successfully`,
+                })
+            } else {
+                res.status(500).json({
+                    del: false,
+                    message: `the id ${id} is no longer available in database`
+                })
+            }
         } catch (error) {
             res.status(500).json({
                 del: false,
