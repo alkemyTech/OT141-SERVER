a9e581b4b7178b79fc9e906e2ff9676e5cbbb16d
fedapon <fedapon@gmail.com>
2022-03-04 16:48:20 -0300

    fix require with outdated user controller name

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const userController = require("../controllers/user.controller"); 
-const { deleteUser } = require("../controllers/users");
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
