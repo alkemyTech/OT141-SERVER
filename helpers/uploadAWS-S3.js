8af1099d194bec9de58381931a58b42c42cba4b3
Emanuel Arroyo <emanuelarroyodev@gmail.com>
2022-03-05 00:17:10 -0300

    Add Ticket 39 | Setup AMAZON S3

---
+++ helpers/uploadAWS-S3.js
@@ -0,0 +1,35 @@
+const S3 = require('aws-sdk/clients/s3');
+
+// Environment Variables
+const region = process.env.AWS_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+const bucketName = process.env.AWS_NAME_BUCKET;
+
+const storage = new S3({
+  // We create an S3 instance passing it the required
+  // parameters to establish the connection to AWS S3
+  region,
+  accessKeyId,
+  secretAccessKey,
+});
+
+// Receives a file and a string with the name of a directory that
+// represents the environment where the function will be used
+const uploadInBucket = (file, directoryNameInBucket) => {
+  if (file.length > 1) throw new Error('The function receives only one file');
+
+  // Using the library that is with this code we look for the file that we
+  // need to send to the Body parameter
+  const Body = Object.values(file).find((value) => Buffer.isBuffer(value));
+
+  return storage
+    .upload({
+      Bucket: bucketName, // Bucket Name
+      Key: `${directoryNameInBucket}/${file.name}`, // The Key is the value with which it is saved in the Bucket
+      Body, // The Body receives the binary information from the file
+    })
+    .promise(); // return as promise
+};
+
+module.exports = { uploadInBucket };
