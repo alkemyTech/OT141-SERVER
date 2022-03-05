a754859341e41b83ed4ad363c414f8c20f523d43
fedapon <fedapon@gmail.com>
2022-03-05 12:10:04 -0300

    create function to add any amount of admin and regular users

--- seeders/20201109013159-create-demo-user.js
+++ seeders/20201109013159-create-demo-user.js
@@ -1,17 +1,45 @@
 /* eslint-disable */
+const {ROLE_ADMIN, ROLE_USER} = require('../constants/user.constants')
+const password = require('../helpers/password')
+
+const NUMBER_OF_ADMIN_USERS = 10
+const NUMBER_OF_REGULAR_USERS = 10
+
+async function createDemoUsers (numberOfAdmin, numberOfUser) {
+  let users = []
+  for (let i=1; i <= numberOfAdmin; i++) {
+    users.push({
+        firstName: `User ${i}`,
+        lastName: 'Admin',
+        email: `admin${i}@test.com`,
+        // Important: Password not encrypted yet!
+        password: await password.encrypt(`admin${i}`),
+        roleId: ROLE_ADMIN,
+        image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    })
+  }
+  for (let i=1; i <= numberOfUser; i++) {
+    users.push({
+        firstName: `User ${i}`,
+        lastName: 'Regular',
+        email: `user${i}@test.com`,
+        // Important: Password not encrypted yet!
+        password: await password.encrypt(`user${i}`),
+        roleId: ROLE_USER,
+        image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    })
+  }
+  return users
+}
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Users', [{
-      firstName: 'Usuario',
-      lastName: 'Demo',
-      email: 'test@test.com',
-      // Important: Password not encrypted yet!
-      password: '1234',
-      roleId: 1,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }], {});
+    const users = await createDemoUsers(NUMBER_OF_ADMIN_USERS, NUMBER_OF_REGULAR_USERS);
+    await queryInterface.bulkInsert('Users', users, {});
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -19,7 +47,7 @@ module.exports = {
      * Add commands to revert seed here.
      *
      * Example:
-     * await queryInterface.bulkDelete('People', null, {});
+     * await queryInterface.bulkDelete('Users', null, {});
      */
   },
 };
