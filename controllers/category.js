02b0b9303cd47e8ec814b92a868be4cd4af4a87b
galantejulian <galante.julian@gmail.com>
2022-03-08 19:46:10 -0300

    delete categories not tested yet

--- controllers/category.js
+++ controllers/category.js
@@ -102,9 +102,37 @@ const getCategoryById = async (req, res) => {
   }
 };
 
+const deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const categoryDeleted = await db.Categories.destroy({
+      where: {
+        id,
+      },
+    });
+    if (categoryDeleted === 1) {
+      res.status(200).json({
+        del: true,
+        message: `category with id ${id}, was deleted successfully`,
+      });
+    } else {
+      res.status(404).json({
+        del: false,
+        message: `the id ${id} does not correspond to any category`,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      del: false,
+      data: error,
+    });
+  }
+}
+
 module.exports = {
   createCategory,
   getAllCategories,
   updateCategoryById,
   getCategoryById,
+  deleteCategory
 };
