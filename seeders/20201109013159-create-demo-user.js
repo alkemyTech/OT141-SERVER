2b311fc11fe622e657eb27d81760a396a7d68c50
fedapon <fedapon@gmail.com>
2022-03-08 17:56:19 -0300

    refactoring to allow multiple users-roleId

--- seeders/20201109013159-create-demo-user.js
+++ seeders/20201109013159-create-demo-user.js
@@ -2,43 +2,45 @@
 const {ROLE_ADMIN, ROLE_USER} = require('../constants/user.constants')
 const password = require('../helpers/password')
 
-const NUMBER_OF_ADMIN_USERS = 10
-const NUMBER_OF_REGULAR_USERS = 10
+const configUsers = [
+  {
+    roleId : ROLE_ADMIN,
+    quantity: 10,
+    firstName: 'User',
+    lastName: 'Admin',
+    email: 'admin',
+  },
+  {
+    roleId : ROLE_USER,
+    quantity: 10,
+    firstName: 'User',
+    lastName: 'Regular',
+    email: 'user',
+  },
+]
 
-async function createDemoUsers (numberOfAdmin, numberOfUser) {
+async function createDemoUsers (configUsers) {
   let users = []
-  for (let i=1; i <= numberOfAdmin; i++) {
-    users.push({
-        firstName: `User ${i}`,
-        lastName: 'Admin',
-        email: `admin${i}@test.com`,
-        // Important: Password not encrypted yet!
-        password: await password.encrypt(`admin${i}`),
-        roleId: ROLE_ADMIN,
+  for await (const item of configUsers) {
+    for (let i=1; i <= item.quantity; i++) {
+      users.push({
+        firstName: `${item.firstName} ${i}`,
+        lastName: item.lastName,
+        email: `${item.email}${i}@test.com`,
+        password: await password.encrypt(`${item.email}${i}`),
+        roleId: item.roleId,
         image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
         createdAt: new Date(),
         updatedAt: new Date(),
-    })
-  }
-  for (let i=1; i <= numberOfUser; i++) {
-    users.push({
-        firstName: `User ${i}`,
-        lastName: 'Regular',
-        email: `user${i}@test.com`,
-        // Important: Password not encrypted yet!
-        password: await password.encrypt(`user${i}`),
-        roleId: ROLE_USER,
-        image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    })
+      })
+    }
   }
   return users
 }
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const users = await createDemoUsers(NUMBER_OF_ADMIN_USERS, NUMBER_OF_REGULAR_USERS);
+    const users = await createDemoUsers(configUsers);
     await queryInterface.bulkInsert('Users', users, {});
   },
 
