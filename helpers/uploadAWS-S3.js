a3276d83a9452c2151f0321f2a18be9ee7530854
Emanuel Arroyo <emanuelarroyodev@gmail.com>
2022-03-09 13:23:48 -0300

    Completed Ticket 39

--- helpers/uploadAWS-S3.js
+++ helpers/uploadAWS-S3.js
@@ -14,24 +14,10 @@ const storage = new S3({
   secretAccessKey,
 });
 
-// Receives a file and a string with the name of a directory that
-// represents the environment where the function will be used
-
-const uploadInBucket = (file, directoryNameInBucket) => {
-  if (typeof file !== 'object' || Array.isArray(file)) throw new Error('The first parameter has to be an object');
-  if (typeof directoryNameInBucket !== 'string') throw new Error('The second parameter has to be a string');
-  // Using the library that is with this code we look for the file that we
-  // need to send to the Body parameter
-  const Body = Object.values(file).find((value) => Buffer.isBuffer(value));
-  if (!Body) throw new Error('The file is invalid');
-
-  return storage
-    .upload({
-      Bucket: bucketName, // Bucket Name
-      Key: `${directoryNameInBucket}/${file.name}`, // The Key is the value with which it is saved in the Bucket
-      Body, // The Body receives the binary information from the file
-    })
-    .promise(); // return as promise
-};
+const uploadInBucket = (fileName, body) => storage.upload({
+  Bucket: bucketName, // Bucket Name
+  Key: fileName, // The Key is the value with which it is saved in the Bucket
+  Body: body, // The Body receives the binary information from the file
+}).promise(); // return as promise
 
 module.exports = { uploadInBucket };
