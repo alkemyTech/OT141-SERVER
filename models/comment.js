da797e24088603ef5d1ff7b357c7883302eaf8ce
fedapon <fedapon@gmail.com>
2022-03-09 21:24:36 -0300

    Comment model and migration created

---
+++ models/comment.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+    }
+  }
+  Comment.init({
+    user_id: DataTypes.INTEGER,
+    body: DataTypes.TEXT,
+    post_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Comment',
+  });
+  return Comment;
+};
