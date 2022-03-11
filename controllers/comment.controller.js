0e607ed88215d2eb0ed988c0960ca4dc9c771d12
fedapon <fedapon@gmail.com>
2022-03-10 23:46:17 -0300

    added create function to the comment controller

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -20,4 +20,22 @@ module.exports = {
       });
     }
   },
+  create: async (req, res) => {
+    try {
+      const { post_id, user_id, body } = req.body; // eslint-disable-line
+      const newComment = await db.Comment.create({
+        post_id, // eslint-disable-line
+        user_id, // eslint-disable-line
+        body,
+      });
+      return res.status(201).json({
+        message: 'comment created',
+        comment: newComment,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: 'internal server error',
+      });
+    }
+  },
 };
