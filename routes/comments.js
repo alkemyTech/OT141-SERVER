a8ff6237bfdf7cd1ca1774bd91fe6f6e0cf4045a
fedapon <fedapon@gmail.com>
2022-03-10 23:45:29 -0300

    post endpoint created and its validation

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const commentController = require('../controllers/comment.controller');
+const { createCommentValidation } = require('../middlewares/comment.validations');
 const {
+  validateErrors,
   verifyToken,
   checkAdminRole,
 } = require('../middlewares');
@@ -11,6 +13,13 @@ router.get(
   verifyToken,
   checkAdminRole,
   commentController.list,
+);
+
+router.post(
+  '/',
+  createCommentValidation,
+  validateErrors,
+  commentController.create,
 );
 
 module.exports = router;
