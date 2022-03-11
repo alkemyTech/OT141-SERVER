1f494271be456567d9e2734d77589886846bf3ce
Facundo Montenegro <facumonte29@gmail.com>
2022-03-11 08:06:03 -0300

    add testimony model and migration

---
+++ migrations/20220311105754-create-testimony.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Testimonials', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Testimonials');
+  }
+};
---
+++ models/testimony.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Testimony extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Testimony.init({
+    name: DataTypes.STRING,
+    image: DataTypes.STRING,
+    content: DataTypes.TEXT,
+  }, {
+    sequelize,
+    modelName: 'Testimony',
+  });
+  return Testimony;
+};
