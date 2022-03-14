d313dc5bcbbe34376dc52916a797d39731381404
galantejulian <galante.julian@gmail.com>
2022-03-14 15:01:00 -0300

    final changes

--- controllers/member.js
+++ controllers/member.js
@@ -4,24 +4,25 @@ module.exports = {
         const { id } = req.params;
         const { name, image } = req.body;
         try {
-            const memberFound = await db.Member.findByPk(id);
-            if (!memberFound) {
-                res.status(404).json({
+            let member = await db.Member.update({
+                name,
+                image,
+            }, {
+                where: { id },
+            });
+
+            if (!member[0]) {
+                return res.status(404).json({
                     ok: false,
                     msg: 'there is no member matching the specified id',
                 });
-            } else {
-                await db.Member.update({
-                    name,
-                    image,
-                }, {
-                    where: { id },
-                });
-                res.status(200).json({
-                    ok: true,
-                    msg: 'member updated successfully',
-                });
+
             }
+            res.status(200).json({
+                ok: true,
+                msg: 'member updated successfully',
+            });
+
         } catch (error) {
             res.status(500).json({
                 msg: 'an error occurred',
