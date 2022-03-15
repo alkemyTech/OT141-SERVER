c98c0650372268db488eff056420de3087b3a977
Nathan Delgado <nathandelgado.dv@gmail.com>
2022-03-15 17:40:37 -0500

    Remove image validations

--- middlewares/members.js
+++ middlewares/members.js
@@ -3,8 +3,6 @@ const { check } = require('express-validator');
 const newMemberValidations = [
   check('name', 'The value is required').not().isEmpty(),
   check('name', 'The value should be type string').isString(),
-  check('image', 'The value is required').not().isEmpty(),
-  check('image', 'The value not is a string').isString(),
 ];
 
 module.exports = {
