febf57f5307e18fbee8f343033329d40d4846a0d
fedapon <fedapon@gmail.com>
2022-03-17 19:18:11 -0300

    fix createMember

--- controllers/member.js
+++ controllers/member.js
@@ -48,4 +48,22 @@ module.exports = {
       });
     }
   },
+  createMember: async (req, res) => {
+    try {
+      const { name, image } = req.body;
+      const newMember = await db.member.create({
+        name,
+        image,
+      });
+      return res.status(201).json({
+        ok: true,
+        msg: `Created ${newMember.name} member`,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        ok: false,
+        msg: 'Contact to administrator',
+      });
+    }
+  },
 };
