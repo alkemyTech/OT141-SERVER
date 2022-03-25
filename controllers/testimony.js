0dc5236f08ed4108319948952b7305c4e2aeda28
Emanuel Arroyo <emanuelarroyodev@gmail.com>
2022-03-25 17:06:41 -0300

    respecting scope |||||||||||||||||||||||||||

--- controllers/testimony.js
+++ controllers/testimony.js
@@ -65,7 +65,36 @@ const updateTestimony = async (req, res) => {
   }
 };
 
+const removeTestimony = async (req, res) => {
+  try {
+    const { id } = req.params;
+    // As paranoid at model is setted to true, destroy method will make a soft delete;
+    const TestimonialDeleted = await db.Testimonial.destroy({
+      where: {
+        id,
+      },
+    });
+    if (TestimonialDeleted) {
+      res.status(200).json({
+        ok: true,
+        message: 'The testimony deleted successfully.',
+      });
+    } else {
+      res.status(404).json({
+        ok: false,
+        message: 'The testimony does not exist.',
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   createTestimony,
   updateTestimony,
+  removeTestimony,
 };
