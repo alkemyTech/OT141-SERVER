2ef20f188668c03ff68f336cb72b2640d70afea2
Facundo Montenegro <facumonte29@gmail.com>
2022-03-27 17:44:31 -0300

    fix partially auth.test

--- tests/endpoints/auth.test.js
+++ tests/endpoints/auth.test.js
@@ -1,9 +1,11 @@
 /* eslint-disable */
+const supertest = require('supertest');
 const request = require('supertest');
 const app = require('../../app');
 const db = require('../../models');
 const { ROLE_ADMIN, ROLE_USER } = require('../../constants/user.constants');
 const { body } = require('express-validator');
+const testimonialsTestHelpers = require('../testimonials.test.helpers');
 
 const server = app.listen(process.env.PORT_TEST);
 
@@ -13,17 +15,17 @@ beforeEach(async () => {
   });
 });
 
-  const user = {
-    firstName: 'user',
-    lastName: 'test',
-    email: 'usertest@test.com',
-    password: 'Test123**',
-    roleId:ROLE_USER,
-  };
+const user = {
+  firstName: 'username',
+  lastName: 'testLastName',
+  email: 'usertest@test.com',
+  password: 'Test123**',
+  roleId: ROLE_USER,
+};
 
 describe('POST/auth/register', () => {
 
-  test('should return 201 status code, token, message and user', async () => { 
+  test('should return 201 status code, token, message and user', async () => {
 
     const result = await request(server)
       .post('/auth/register')
@@ -34,9 +36,8 @@ describe('POST/auth/register', () => {
     expect(result.body.user).toBeDefined();
     expect(result.body.message).toBeDefined();
     expect(result.body.message).toEqual('User created successfully');
-    expect(result.body.user.firstName).toEqual('user1');
-    expect(result.body.user.firstName).toEqual('user1');
-    expect(result.body.user.lastName).toEqual('test1');
+    expect(result.body.user.firstName).toEqual('username');
+    expect(result.body.user.lastName).toEqual('testLastName');
     expect(result.body.user.email).toEqual('usertest@test.com');   
   });
 
@@ -47,12 +48,12 @@ describe('POST/auth/register', () => {
       .send(user)
       .expect('Content-type', /application\/json/);
 
-      await request(server)
+    await request(server)
       .post('/auth/register')
       .expect('Content-type', /application\/json/);
-  });  
+  });
 
-  test('Should return 422 status code and message if request no contain all required params',  async () => {
+  test('Should return 422 status code and message if request no contain all required params', async () => {
     const user = {
       firstName: '',
       lastName: '',
@@ -65,19 +66,19 @@ describe('POST/auth/register', () => {
       .send(user)
       .expect(422);
 
-      expect(result.body.errors).toBeDefined(); 
-      expect(result.body.errors.length).toBe(4); 
-    });  
+    expect(result.body.errors).toBeDefined();
+    expect(result.body.errors.length).toBe(4);
+  });
 
-  test('should return 200 http code and message if exist email', async () => { 
+  test('should return 200 http code and message if exist email', async () => {
     await request(server)
       .post('/auth/register')
       .send(user);
 
     const result = await request(server)
-    .post('/auth/register')
-    .send(user)
-    .expect(200);
+      .post('/auth/register')
+      .send(user)
+      .expect(200);
 
     expect(result.body.message).toBeDefined();
     expect(result.body.message).toEqual('Email already in use');
@@ -109,7 +110,7 @@ describe('POST/auth/login', () => {
     await request(server)
       .post('/auth/register')
       .send(user)
-    
+
     const result = await request(server)
       .post('/auth/login')
       .send(userLogin)
@@ -120,7 +121,7 @@ describe('POST/auth/login', () => {
 
   test('should return 400 status code and msg if is incorrect password', async () => {
     await db.User.create(user);
-    
+
     const userIncorrectPassword = {
       email: 'usertest@test.com',
       password: 'Test123',
@@ -137,7 +138,7 @@ describe('POST/auth/login', () => {
 
   test('should return 400 status code an msg if is empty password', async () => {
     await db.User.create(user);
-    
+
     const userEmptyPassword = {
       email: 'usertest@test.com',
       password: '',
@@ -181,7 +182,7 @@ describe('POST/auth/login', () => {
     expect(result.body.msg).toBeDefined();
     expect(result.body.msg).toEqual('The  not exist');
   });
-  
+
   test('should return 404 status code if email is empty and password', async () => {
     const notExistParams = {
       email: '',
@@ -200,7 +201,6 @@ describe('POST/auth/login', () => {
 
 describe('GET/auth/me', () => {
 
-
   test('should return 403 code and should be aplication/json if not exist token', async () => {
     const result = await request(server)
       .get('/auth/me')
@@ -210,22 +210,20 @@ describe('GET/auth/me', () => {
     expect(result.body.error).toContain('A token is required for authentication');
   });
 
-  test('should return 200 status code and usuario', async () => { 
+  test('should return 200 status code and usuario', async () => {
 
     const userLogin = {
       email: 'usertest@test.com',
-      password: 'Test123**',
+      password: 'Test123**'
     };
-    const {body: { token }} = await request(server)
+    const { body: { token } } = await request(server)
       .post('/auth/login')
       .send(userLogin);
-    console.log(token)
-    
+
     const result = await request(server)
       .get('/auth/me')
-      .auth(token, { type: "bearer" } )
+      .auth(token, { type: "bearer" })
       .expect(200);
-    
     expect(result.body.usuario).toBeDefined();
     expect(result.body.usuario.email).toEqual('usertest@test.com');
   });
